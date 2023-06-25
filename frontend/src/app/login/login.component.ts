766dea7a57fcf5eec53f18bcbf71d60d72d2c100
Kenan Omic <kenanomic@192.168.1.7>
2023-06-25 11:32:43 +0200

    updated login css

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  login() {
+    const url = 'http://localhost:8000/login';
+
+    // Assuming you have the username and password values from the input fields
+    const username = 'your_username';
+    const password = 'your_password';
+
+    // Make the HTTP POST request
+    this.http.post(url, { username, password }).subscribe(
+      response => {
+        console.log('Response:', response);
+        // Handle the response as needed
+      },
+      error => {
+        console.error('Error:', error);
+        // Handle the error as needed
+      }
+    );
+  }
+
 
 }
