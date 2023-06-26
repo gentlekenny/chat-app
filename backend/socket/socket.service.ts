3cd7bdd7aa70d158a6671fd03fd2ffb063d31e3e
Kenan Omic <kenanomic@192.168.1.7>
2023-06-26 14:31:34 +0200

    fixed bug when sending messages

--- backend/socket/socket.service.ts
+++ backend/socket/socket.service.ts
@@ -4,7 +4,7 @@ import Chatroom from "../modules/chatroom/chatroom.interface";
 
 export const handleSocketEvents = (socket: Socket, db: Db) => {
     socket.on("send-message", (data) => {
-        socket.broadcast.emit("chat-message", data.user + ": " + data.message)
+        socket.broadcast.emit("chat-message", data.sender + ": " + data.context)
         // Have to save messages into the database, but this can be done behind the scenes
         // so no need to send a post request, socket can handle it
         db.collection("messages").insertOne(data)
