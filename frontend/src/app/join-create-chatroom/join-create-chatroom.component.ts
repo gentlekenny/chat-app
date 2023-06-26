36bc634f62fe5baf1fd02a178a18e89399676a85
Kenan Omic <kenanomic@192.168.1.7>
2023-06-26 15:32:53 +0200

    added validation for creating chatrooms

--- frontend/src/app/join-create-chatroom/join-create-chatroom.component.ts
+++ frontend/src/app/join-create-chatroom/join-create-chatroom.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -13,15 +14,33 @@ export class JoinCreateChatroomComponent {
   @Input() public user: string = ""
   @Input() public socket: Socket | null = null
 
+  constructor(private snackbar: MatSnackBar) {
+
+  }
+
   chatroomName: string = ""
 
   joinChatroomOnClick() {
+    if (this.chatroomName.length <= 4 || this.chatroomName.indexOf(' ') > 0) {
+      this.inputError()
+      return
+    }
     const chatroom = {
       name: this.chatroomName,
       users: [this.user],
       totalMembers: 1
     }
     this.socket?.emit("chatroom-created", (chatroom))
+    this.chatroomName = ''
   }
 
+  inputError() {
+    const config: MatSnackBarConfig = {
+      duration: 3000, // Duration of info window
+      panelClass: ['error-snackbar'], // Using this winow style
+      verticalPosition: 'top'
+    };
+    this.snackbar.open(`Error: Chatroom name must be atleast 5 characters long and must not contain whitespaces.`, '', config);
+    this.chatroomName = ''
+  }
 }
